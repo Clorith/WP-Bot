303ea58976c5854dcfa7589d72401816ad7c32c4
Clorith <bg3erl42>
2015-03-11 15:35:55 +0100

    Add jQuery to the project

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,4 @@
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 </html>

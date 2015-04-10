6799e6bb2e1087d0d971f330cb3a833f2aed655b
Clorith <bg3erl42>
2015-04-10 15:41:28 +0200

    We truncate existing stats (why did we keep it to begin with) so no need to check for updateable rows no more. We also don't need to include doc-bot in the stats.

--- cron/stats-30d.php
+++ cron/stats-30d.php
@@ -22,14 +22,12 @@ $messages = $db->query( "
 		m.message,
 		m.is_question,
 		m.is_appreciation,
-		m.is_docbot,
-		s.nickname AS stats
+		m.is_docbot
 	FROM
 		messages m
-	LEFT JOIN
-		stats_30d s
-			ON ( s.nickname = m.nickname )
 	WHERE
+		m.nickname != 'doc-bot'
+	AND
 		m.event = 'message'
 	AND
 		m.time BETWEEN " . $db->quote( date( "Y-m-d 00:00:00", strtotime( '-1 month', time() ) ) ) . " AND " . $db->quote( date( "Y-m-d h:i:s", time() ) ) . "
@@ -42,8 +40,7 @@ while ( $message = $messages->fetchObject() ) {
 			'messages'     => 0,
 			'questions'    => 0,
 			'appreciation' => 0,
-			'docbot'       => 0,
-			'has_stats'    => ( ! empty( $message->stats ) )
+			'docbot'       => 0
 		);
 	}
 
@@ -60,45 +57,25 @@ while ( $message = $messages->fetchObject() ) {
 }
 
 foreach( $nicknames AS $nickname => $stats ) {
-	if ( ! $stats['has_stats'] ) {
-		try {
-			$db->query( "
-			INSERT INTO
-				stats_30d (
-					nickname,
-					messages,
-					appreciation,
-					questions,
-					docbot
-				)
-			VALUES (
-				" . $db->quote( $nickname ) . ",
-				" . $db->quote( $stats['messages'] ) . ",
-				" . $db->quote( $stats['appreciation'] ) . ",
-				" . $db->quote( $stats['questions'] ) . ",
-				" . $db->quote( $stats['docbot'] ) . "
-			)
-		" );
-		} catch ( PDOException $e ) {
-			$stats['has_stats'] = true;
-		}
-	}
-
-	if ( $stats['has_stats'] ) {
+	try {
 		$db->query( "
-			UPDATE
-				stats_30d
-			SET
-				messages = " . $db->quote( $stats['messages'] ) . ",
-				appreciation = " . $db->quote( $stats['appreciation'] ) . ",
-				questions = " . $db->quote( $stats['questions'] ) . ",
-				docbot = " . $db->quote( $stats['docbot'] ) . "
-			WHERE
-				nickname = " . $db->quote( $nickname ) . "
-			LIMIT 1
-		" );
-	}
-	else {
+		INSERT INTO
+			stats_30d (
+				nickname,
+				messages,
+				appreciation,
+				questions,
+				docbot
+			)
+		VALUES (
+			" . $db->quote( $nickname ) . ",
+			" . $db->quote( $stats['messages'] ) . ",
+			" . $db->quote( $stats['appreciation'] ) . ",
+			" . $db->quote( $stats['questions'] ) . ",
+			" . $db->quote( $stats['docbot'] ) . "
+		)
+	" );
+	} catch ( PDOException $e ) {
 
 	}
 }

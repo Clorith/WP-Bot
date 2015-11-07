170f428c9dd0e66f77dce928501fb79a2a6d6fcd
Clorith <bg3erl42>
2015-11-07 03:29:03 +0100

    Pull static messages out of the source code

--- IRC/doc-bot.php
+++ IRC/doc-bot.php
@@ -9,10 +9,6 @@
 class DocBot {
 	private $plugin_details = array();
 
-	function __construct() {
-
-	}
-
 	function message_split( $data ) {
 		$message_parse = explode( ' ', $data->message, 2 );
 		$command = $message_parse[0];
@@ -208,29 +204,6 @@ class DocBot {
 		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
 	}
 
-	function pastebin( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Please post your source code with a service like %s or similar for us to look at, and avoid pasting large pieces of code to the channel.',
-			$msg->user,
-			'http://gist.github.com'
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function do_the_first( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Please attempt to disable all plugins, and use one of the default (Twenty*) themes. If the problem goes away, enable them one by one to identify the source of your troubles.',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
 	function language( &$irc, &$data ) {
 		$msg = $this->message_split( $data );
 
@@ -238,50 +211,6 @@ class DocBot {
 			'%s: Please help us keep %s a family friendly room, and avoid using foul language.',
 			$msg->user,
 			$data->channel
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function moving( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: If you rename the WordPress directory on your server, switch ports or change the hostname http://codex.wordpress.org/Moving_WordPress applies',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function inspector( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Please use the built-in Developer Tools of your browser to fix problems with your website. Right click your page and pick “Inspect Element” (Cr, FF, Op) or press F12-button (IE) to track down CSS problems. Use the console to see JavaScript bugs.',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function wordpresscom( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: For support with your blog or site hosted on WordPress.com, please see http://en.support.wordpress.com/contact/',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function make_blog( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: To get started contributing to WordPress, have a look at the Make Blogs over at https://make.wordpress.org',
-			$msg->user
 		);
 
 		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
@@ -298,138 +227,6 @@ class DocBot {
 		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
 	}
 
-	function underscores( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Check out http://underscores.me/ - Underscores is a starter theme meant to be the base of your next awesome theme, try it out!',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function lucky_seven( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Setting file permissions to 777 is inherently insecure, please read http://codex.wordpress.org/Changing_File_Permissions',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function ftp( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s:  If WordPress keeps asking you for FTP credentials see http://codex.wordpress.org/Updating_WordPress#Automatic_Update and http://codex.wordpress.org/Editing_wp-config.php#WordPress_Upgrade_Constants as well as http://s.sivel.net/wpfsmethod for more information about the file system method selection and http://v007.me/4 for forcing direct file system writes',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function hacked( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: http://codex.wordpress.org/FAQ_My_site_was_hacked , and stop trying to patch up your hacked site. Reinstall or restore your backups. And read http://codex.wordpress.org/Hardening_WordPress',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function next( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Another happy customer leaves the building :)',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function related( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: It\'s not a WordPress question just because the user uses WordPress',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function css( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Please /join #css for questions about CSS',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function html( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Please /join #html for questions about HTML',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function javascript( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Please /join #javascript for questions about javascript',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function php( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Please /join ##php for questions about PHP',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function possible( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Everything is "possible" - If you have questions about how to do something specific, then feel free to ask',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function pages( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: "Pages" in WordPress are innocuous. There are the proper terms for each:  WordPress pages (those are made in the dashboard under add new page) Site Pages (those are whatever exists on the front end of your site but have no WordPress Page such as archive pages, the 404, etc.) and Page Templates (php files you can apply to a WordPress page)',
-			$msg->user
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
 	function md5( &$irc, &$data ) {
 		$msg = $this->message_split( $data );
 
@@ -437,17 +234,6 @@ class DocBot {
 			'%s: %s',
 			$msg->user,
 			md5( $msg->message )
-		);
-
-		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );
-	}
-
-	function ask( &$irc, &$data ) {
-		$msg = $this->message_split( $data );
-
-		$message = sprintf(
-			'%s: Don\'t ask to ask, just ask :)',
-			$msg->user
 		);
 
 		$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, $message );

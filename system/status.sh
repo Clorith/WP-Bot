a2f0cc3c6b8abdf3d825e1ec3743ab47fa99c7fc
Clorith <bg3erl42>
2016-05-03 22:41:26 +0200

    Add system and a shell script for detecting if the bot is running, and restart it if required

---
+++ system/status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script for detecting if our PHP bot is running, or rebooting it if it isn't
+
+PHP_PROCESSES="$(ps -C php --no-headers | wc -l)"
+WPBOT_SCREEN="$(screen -ls | grep -q WPBot)"
+
+if PHP_PROCESSES == 0 then
+  # If no screen session exists, start one
+  if ! WPBOT_SCREEN then
+    screen -mdS WPBot
+  fi
+
+  # Create a WPBot instance in the screen window
+  screen -S WPBot -p 0 -X stuff "/usr/bin/php /home/contribot/IRC/contributor-bot.php$(printf \\r)"
+fi
